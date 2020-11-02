89058260d5e6292ddbbda322ae02a95f892410fd
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-01 20:21:59 -0500

    [app] Updated operation logic, artifacts added.

--- app.js
+++ app.js
@@ -26,6 +26,40 @@ const consLog = () => {
 // Divide Result by Min or Max
 // This was it avoids remainders. 
 
+
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+//TESTING add function
+// console.log(add(1, 5));
+// console.log(add(5,8));
+// console.log(add(10, 11));
+// console.log(add(9,8));
+
+
+const subtract = (num1, num2) => {
+    return num2 > num1 ? num2 - num1 : num1 - num2;
+}
+
+//TESTING subtract function
+// console.log(subtract(1, 5));
+// console.log(subtract(5,8));
+// console.log(subtract(10, 11));
+// console.log(subtract(9,8));
+
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+//TESTING multiply function
+// console.log(multiply(1, 5));
+// console.log(multiply(5,8));
+// console.log(multiply(10, 11));
+// console.log(multiply(9,8));
+
 const divide = (num1, num2) => {
     let result = num1 * num2;
     if (divisionSwitch === true) {
@@ -38,14 +72,14 @@ const divide = (num1, num2) => {
 }
 
 //TESTING divide function
-console.log(divide(12,8));
-console.log(divisionSwitch);
-console.log(divide(12,8));
-console.log(divisionSwitch);
-console.log(divide(12,8));
-console.log(divisionSwitch);
-console.log(divide(1,10));
-console.log(divisionSwitch);
+// console.log(divide(12,8));
+// console.log(divisionSwitch);
+// console.log(divide(12,8));
+// console.log(divisionSwitch);
+// console.log(divide(12,8));
+// console.log(divisionSwitch);
+// console.log(divide(1,10));
+// console.log(divisionSwitch);
 
 
 
